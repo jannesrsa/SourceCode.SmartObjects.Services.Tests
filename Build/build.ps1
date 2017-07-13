b99d75cc2d8a1965b19a53b8848a0bc2dcbf4869
Jannes Coetzee <jannes@k2.com>
2017-07-13 19:29:16 +0200

    Update build.ps1 to update all vstest.console.exe.config

--- Build/build.ps1
+++ Build/build.ps1
@@ -1,15 +1,26 @@
 [Reflection.Assembly]::LoadWithPartialName("System.Xml.Linq") | Out-Null
-  
-$pathToConfig = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe.config"
-$xDoc = [System.Xml.Linq.XDocument]::Load($pathToConfig)
-$configSections = $xDoc.Root.Element("configSections")
 
-$startupXml = [System.Xml.Linq.XElement]::Parse('<startup useLegacyV2RuntimeActivationPolicy="true"> 
-       <supportedRuntime version="v4.0"/>
-  </startup>')
+$files = Get-ChildItem -Path C:\ -Filter vstest.console.exe.config -Recurse -ErrorAction SilentlyContinue -Force
+foreach ($file in $files) {
+    $vstestConfigXDocument = [System.Xml.Linq.XDocument]::Load($file.FullName)
+    if(!$vstestConfigXDocument.Root.Element("startup"))
+    {
+         $startupXElement = [System.Xml.Linq.XElement]::Parse('<startup useLegacyV2RuntimeActivationPolicy="true"> 
+                <supportedRuntime version="v4.2"/>
+            </startup>')
 
-$configSections.AddAfterSelf($startupXml)
-$xDoc.Save($pathToConfig)
+        $file.FullName
+        $configSectionsXElement = $vstestConfigXDocument.Root.Element("configSections")
+       
+        if($configSectionsXElement)
+        {
+            $configSectionsXElement.AddAfterSelf($startupXElement)
+        }
+        else
+        {
+            $vstestConfigXDocument.Root.Add($startupXElement)
+        }
 
-$xDoc2 = [System.Xml.Linq.XDocument]::Load($pathToConfig)
-$xDoc2.ToString()
+        $vstestConfigXDocument.Save($file.FullName)
+    }
+}
