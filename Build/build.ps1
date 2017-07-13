fa36c29fe1091fe22905b033fb0c8ea6fde1814e
Jannes Coetzee <jannes@k2.com>
2017-07-13 21:16:48 +0200

    FusionLog

--- Build/build.ps1
+++ Build/build.ps1
@@ -1,3 +1,8 @@
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Fusion -Name ForceLog         -Value 1               -Type DWord
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Fusion -Name LogFailures      -Value 1               -Type DWord
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Fusion -Name LogResourceBinds -Value 1               -Type DWord
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Fusion -Name LogPath          -Value 'C:\FusionLog\' -Type String
+
 [Reflection.Assembly]::LoadWithPartialName("System.Xml.Linq") | Out-Null
 
 $files = Get-ChildItem -Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\" -Filter vstest.console.exe.config -Recurse -ErrorAction SilentlyContinue -Force
