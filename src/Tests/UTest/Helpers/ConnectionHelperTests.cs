3810ad5a0b3bddd13c7180bf4752c9225292baa3
Jannes Coetzee <jannes@k2.com>
2017-07-14 09:03:11 +0200

    MSTest.TestAdapter.1.1.18

--- src/Tests/UTest/Helpers/ConnectionHelperTests.cs
+++ src/Tests/UTest/Helpers/ConnectionHelperTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SourceCode.SmartObjects.Services.Tests.Helpers.Tests
 {
@@ -10,26 +7,6 @@ namespace SourceCode.SmartObjects.Services.Tests.Helpers.Tests
     {
         [TestMethod()]
         public void GetCurrentUserTest()
-        {
-            try
-            {
-                AnotherMethod();
-            }
-            catch (Exception ex)
-            {
-                if (ex is FileNotFoundException)
-                {
-                    Debug.WriteLine($"Fusionlog: {(ex as FileNotFoundException).FusionLog}");
-                    Debug.WriteLine($"FileName: {(ex as FileNotFoundException).FileName}");
-                }
-                
-                Debug.WriteLine($"ToString: {ex.ToString()}");
-
-                throw new Exception(ex.ToString());
-            }
-        }
-
-        private static void AnotherMethod()
         {
             // Action
             var currentuser = ConnectionHelper.GetCurrentUser();
