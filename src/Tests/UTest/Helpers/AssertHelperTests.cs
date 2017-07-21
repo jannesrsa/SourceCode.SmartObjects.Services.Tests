d626b373e8ab62787ab07a69792d8a8f26824f93
Jannes Coetzee <jannes@k2.net>
2017-07-21 14:29:05 +0200

    AsertHelperTests

---
+++ src/Tests/UTest/Helpers/AssertHelperTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SourceCode.SmartObjects.Services.Tests.Helpers.Tests
+{
+    [TestClass()]
+    public class AssertHelperTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(Exception))]
+        public void Fail()
+        {
+            AssertHelper.Fail(Guid.NewGuid().ToString());
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exception))]
+        public void IsTrue_WithFalse()
+        {
+            AssertHelper.IsTrue(false, Guid.NewGuid().ToString());
+        }
+
+        [TestMethod()]
+        public void IsTrue_WithTrue()
+        {
+            AssertHelper.IsTrue(true, Guid.NewGuid().ToString());
+        }
+    }
+}
