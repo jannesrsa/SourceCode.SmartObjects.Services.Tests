c771080263147b45120b2d9160775022876c7de9
Jannes Coetzee <jannes@k2.net>
2017-07-22 18:10:38 +0200

    Unit tests for UriCreator

---
+++ src/Tests/UTest/Helpers/UriCreatorTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SourceCode.SmartObjects.Services.Tests.Helpers.Tests
+{
+    [TestClass()]
+    public class UriCreatorTests
+    {
+        [TestMethod()]
+        public void CreateSanitizedPathUri_WithDefaultValues()
+        {
+            // Arrange
+            var expected = new Uri("http://www.k2.com");
+
+            // Action
+            var actual = UriCreator.CreateSanitizedPathUri(UriKind.Absolute, expected.ToString());
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CreateSanitizedPathUri_WithEmptyString()
+        {
+            // Arrange
+            var pathSegment1 = string.Empty;
+
+            // Action
+            var actual = UriCreator.CreateSanitizedPathUri(UriKind.Absolute, pathSegment1);
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod()]
+        public void CreateSanitizedPathUri_WithNoParams()
+        {
+            // Arrange
+            var pathSegment1 = string.Empty;
+
+            // Action
+            var actual = UriCreator.CreateSanitizedPathUri(UriKind.Absolute);
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod()]
+        public void CreateSanitizedPathUri_WithNullPathSegments()
+        {
+            // Arrange
+            var pathSegment1 = (string)null;
+
+            // Action
+            var actual = UriCreator.CreateSanitizedPathUri(UriKind.Absolute, pathSegment1);
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+    }
+}
