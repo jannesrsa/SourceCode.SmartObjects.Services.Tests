cf5d807b7edf8fc5b8a2e1e397021f3cae3d9c2e
Jannes Coetzee <jannes@k2.net>
2017-07-22 18:36:24 +0200

    ValueHelperTests.GetDotNetType_AllTypes

--- src/Tests/UTest/Helpers/ValueHelperTests.cs
+++ src/Tests/UTest/Helpers/ValueHelperTests.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SourceCode.SmartObjects.Services.Tests.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SourceCode.SmartObjects.Client;
 
 namespace SourceCode.SmartObjects.Services.Tests.Helpers.Tests
 {
@@ -12,16 +8,45 @@ namespace SourceCode.SmartObjects.Services.Tests.Helpers.Tests
     public class ValueHelperTests
     {
         [TestMethod()]
-        public void GetDefaultValue_WithDefaultValues()
+        public void GetDefaultValue_ReferenceType()
         {
             // Arrange
             var type = typeof(string);
-            
+
             // Action
             var actual = ValueHelper.GetDefaultValue(type);
 
             // Assert
             Assert.AreEqual(default(string), actual);
+        }
+
+        [TestMethod()]
+        public void GetDefaultValue_ValueType()
+        {
+            // Arrange
+            var type = typeof(int);
+
+            // Action
+            var actual = ValueHelper.GetDefaultValue(type);
+
+            // Assert
+            Assert.AreEqual(default(int), actual);
+        }
+
+        [TestMethod()]
+        public void GetDotNetType_AllTypes()
+        {
+            // Arrange
+            var propertyTypes = Enum.GetValues(typeof(PropertyType));
+
+            foreach (PropertyType propertyType in propertyTypes)
+            {
+                // Action
+                var type = ValueHelper.GetDotNetType(propertyType);
+
+                // Assert
+                Assert.IsNotNull(type);
+            }
         }
     }
 }
