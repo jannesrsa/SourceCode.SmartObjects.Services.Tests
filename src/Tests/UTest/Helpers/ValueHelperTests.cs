228fda19f9553cebcf5ddb14bb2fa1484e780658
Jannes Coetzee <jannes@k2.net>
2017-07-22 18:19:33 +0200

    ValueHelper

---
+++ src/Tests/UTest/Helpers/ValueHelperTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SourceCode.SmartObjects.Services.Tests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceCode.SmartObjects.Services.Tests.Helpers.Tests
+{
+    [TestClass()]
+    public class ValueHelperTests
+    {
+        [TestMethod()]
+        public void GetDefaultValue_WithDefaultValues()
+        {
+            // Arrange
+            var type = typeof(string);
+            
+            // Action
+            var actual = ValueHelper.GetDefaultValue(type);
+
+            // Assert
+            Assert.AreEqual(default(string), actual);
+        }
+    }
+}
